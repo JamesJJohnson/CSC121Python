a6d38078bcf767910b061c99d6c97be5a06f3658
JJamesJohnson <james.johnsonsfc@gmail.com>
2026-02-03 04:24:38 -0500

    Add files via upload

---
+++ name_case.py
@@ -0,0 +1,7 @@
+person_name = "James Johnson"
+lowercase_name = person_name.lower()
+uppercase_name = person_name.upper()
+titlecase_name = person_name.title()
+print("Lowercase:", lowercase_name)
+print("Uppercase:", uppercase_name)
+print("Titlecase:", titlecase_name)
