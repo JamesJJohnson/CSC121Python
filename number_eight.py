77f6ebc343d969c4905cb2a9e81c8a1484d7deb0
JJamesJohnson <james.johnsonsfc@gmail.com>
2026-02-03 14:08:45 -0500

    Add files via upload

---
+++ number_eight.py
@@ -0,0 +1,9 @@
+# 2-8. Number Eight
+# Addition
+print(5 + 3) # This adds 5 and 3, resulting in 8
+# Subtraction
+print(10 - 2) # This subtracts 2 from 10, resulting in 8
+# Multiplication
+print(2 * 4) # This multiplies 2 and 4, resulting in 8
+# Division
+print(16 / 2) # This divides 16 by 2, resulting in 8
