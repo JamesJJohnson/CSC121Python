cc3c42433de689f30b837157ec7432393042613c
JJamesJohnson <james.johnsonsfc@gmail.com>
2026-02-10 03:13:00 -0500

    Add files via upload

---
+++ motorcycles.py
@@ -0,0 +1,4 @@
+motorcycles = ["Honda CBR600RR", "Ducati Panigale V4", "Harley-Davidson Street Glide"]
+print(f"I would love to own a [Honda CBR600RR].")
+print(f"I would love to own a [Ducati Panigale V4].")
+print(f"I would love to own a [Harley-Davidson Street Glide].")
