a453947766c64558809f7a6d0d6dd658f9906727
JJamesJohnson <james.johnsonsfc@gmail.com>
2026-02-17 20:56:41 -0500

    Add files via upload

--- motorcycles.py
+++ motorcycles.py
@@ -1,4 +1,10 @@
-motorcycles = ["Honda CBR600RR", "Ducati Panigale V4", "Harley-Davidson Street Glide"]
-print(f"I would love to own a [Honda CBR600RR].")
-print(f"I would love to own a [Ducati Panigale V4].")
-print(f"I would love to own a [Harley-Davidson Street Glide].")
+motorcycles = [
+    "Honda CBR600RR",
+    "Ducati Panigale V4",
+    "Harley-Davidson Street Glide"
+]
+
+print(f"I would love to own a {motorcycles[0]}.")
+print(f"I would love to own a {motorcycles[1]}.")
+print(f"I would love to own a {motorcycles[2]}.")
+
